e8860a6b285a6ef142b6cb23d98b3fa2ca8d414c
Michal Obielak <mobielak7@gmial.com>
2018-11-07 22:57:46 +0100

    Dodanie klasy DB

---
+++ lib/DBclass.php
@@ -0,0 +1,26 @@
+﻿<?php
+class DB
+{
+	private $connect;
+	private function __construct()
+	{
+		$dbconf = require 'dbconf.php';
+		$this->connect = mysqli_connect($dbconf['host'], $dbconf['login'], $dbconf['pass'], $dbconf['name']);
+		if(!$conn){
+    		echo ("Connection failed: " . mysqli_connect_error());
+    		return false;
+		}
+	}
+	public function insert($sql)
+	{
+		if(mysqli_query($this->connect, $sql)){
+    		return true;
+		} else {
+    		return false;
+	}
+	}
+	private function __desctruct()
+	{
+		mysqli_close($this->connect);
+	}
+}
