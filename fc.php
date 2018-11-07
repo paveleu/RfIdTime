e4fa5286b1ba113137b09024b13cb44eacb54372
Unknown <pablloo98@gmail.com>
2018-11-07 16:50:22 +0100

    insertlog

--- fc.php
+++ fc.php
@@ -3,59 +3,54 @@
 //glowne funkcjie
 
 function getpracidfromrfid($rfid){
+	$dbconf = require 'dbconf.php';
+	$conn = mysqli_connect($dbconf['host'], $dbconf['login'], $dbconf['pass'], $dbconf['name']);
+	if(!$conn){
+    	echo ("Connection failed: " . mysqli_connect_error());
+    	return false;
+	}
 
-// SELECT `idprac`, `cardrfid` FROM `rfid` LEFT JOIN `pracownicy` ON `pracownicy`.`rf_id` = `rfid`.`id_card` WHERE `cardrfid` = "xxxx" - xxx to rfid karty
-
-$dbconf = require 'dbconf.php';
-
-$conn = mysqli_connect($dbconf['host'], $dbconf['login'], $dbconf['pass'], $dbconf['name']);
-if (!$conn) {
-    echo ("Connection failed: " . mysqli_connect_error());
-    return false;
-}
-
-$sql = 'SELECT `idprac`, `cardrfid` FROM `rfid` LEFT JOIN `pracownicy` ON `pracownicy`.`rf_id` = `rfid`.`id_card` WHERE `cardrfid` = "'.$rfid.'"';
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-$row = mysqli_fetch_assoc($result);
- return $row['idprac'];
-
-} else {
-    return false;
-}
-
-mysqli_close($conn);
-
-
-
+	$sql = 'SELECT `idprac`, `cardrfid` FROM `rfid` LEFT JOIN `pracownicy` ON `pracownicy`.`rf_id` = `rfid`.`id_card` WHERE `cardrfid` = "'.$rfid.'"';
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+	$row = mysqli_fetch_assoc($result);
+	return $row['idprac'];
+	}else{
+    	return false;
+	}
+	mysqli_close($conn);
 }
 
 
 function czywpracy($pracid){
-
  	$dbconf = require 'dbconf.php';
-
-$conn = mysqli_connect($dbconf['host'], $dbconf['login'], $dbconf['pass'], $dbconf['name']);
-if (!$conn) {
-    echo ("Connection failed: " . mysqli_connect_error());
-    return false;
+	$conn = mysqli_connect($dbconf['host'], $dbconf['login'], $dbconf['pass'], $dbconf['name']);
+	if(!$conn){
+   		echo ("Connection failed: " . mysqli_connect_error());
+    	return false;
+	}
+	$sql = 'SELECT * FROM log WHERE idprac = "'.$pracid.'" ORDER BY idlog DESC LIMIT 1';
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+		$row = mysqli_fetch_assoc($result);
+ 		return [$row['idoper'], $row['data']];
+	}else{
+    	return [2];
+	}
+	mysqli_close($conn); 
 }
 
-$sql = 'SELECT * FROM log WHERE idprac = "'.$pracid.'" ORDER BY idlog DESC LIMIT 1';
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-$row = mysqli_fetch_assoc($result);
- return [$row['idoper'], $row['data']];
-
-} else {
-    return 1;
+function insertlog($pracid, $stan ,$data){
+	$dbconf = require 'dbconf.php';
+	$conn = new mysqli($dbconf['host'], $dbconf['login'], $dbconf['pass'], $dbconf['name']);
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	} 
+	$sql = 'INSERT INTO `log` (`idlog`, `idprac`, `data`, `idoper`) VALUES (NULL, '.$pracid.', "'.$data.'", '.$stan.')';
+	if($conn->query($sql) === TRUE){
+    	return true;
+	}else{
+    	return false;
+	}
+	$conn->close();
 }
-
-mysqli_close($conn);
-
-
-
- 
-}
-
-// SELECT * FROM log WHERE idprac = "2" ORDER BY idlog DESC LIMIT 1 - czy w pracy
--- log.php
+++ log.php
@@ -3,11 +3,16 @@
 	require_once 'fc.php';
 
 if(isset($_GET['rfid'])){
-	
+	if(isset($_GET['data'])) $data = $_GET['data'];
+	else $data = date("Y-m-d H:i:s");
+
 	$pracid = getpracidfromrfid($_GET['rfid']); //test czy w pracy
 	$czywpracy = czywpracy($pracid);
 		echo $czywpracy[0];
-
+		if($czywpracy[0]==2){
+		 if(insertlog($pracid, 1, $data)) echo "a";
+		}
+		if($czywpracy[0]==1) insertlog($pracid, 2, $data);
 
 	
 
