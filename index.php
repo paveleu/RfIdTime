d62c8d7f6c6d4047e2635cef5071f01fd65b20c0
Unknown <pablloo98@gmail.com>
2018-12-06 23:15:26 +0100

    kosmetyczne poprawy + wyszukiwarka w liście

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ $site = 'login';
   <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>MTCP | Logowanie</title>
+  <title>MTCP</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
